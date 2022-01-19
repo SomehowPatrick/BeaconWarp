ef331e24ee5641552704eda94868c70034884086
UnknownBanana <info@unknownbanana.xyz>
2022-01-19 15:08:35 +0100

    Initial setup

---
+++ src/main/java/com/github/unknownbanana/beaconwarp/BeaconWarp.java
@@ -0,0 +1,10 @@
+package com.github.unknownbanana.beaconwarp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BeaconWarp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
